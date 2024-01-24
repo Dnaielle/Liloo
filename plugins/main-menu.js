f40a238cb1451189f53f179ac33340e3eeb3fab0
Danielle096 <157294641+Danielle096@users.noreply.github.com>
2024-01-24 22:49:38 +0300

    Update main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -255,7 +255,6 @@ import {
    
     global.fcontact = { key: { fromMe: false, participant: `0@s.whatsapp.net`, remoteJid: 'status@broadcast' }, message: { contactMessage: { displayName: `${name}`, vcard: `BEGIN:VCARD\nVERSION:3.0\nN:;a,;;;\nFN:${name}\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}}
     const infoText = `
-    Naksu あ⁩
     اهلاا ${name} سينباي
     
     *${ucpn}* 
