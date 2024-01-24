4d7e06facc79e1e2cd5d2f2b47c386040090306b
Danielle096 <157294641+Danielle096@users.noreply.github.com>
2024-01-24 21:01:41 +0300

    Update bot.js

--- plugins/bot.js
+++ plugins/bot.js
@@ -2,7 +2,7 @@ let handler = async (m, { conn}) => {
 let user = global.db.data.users[m.sender]
 let name = conn.getName(m.sender)
 let taguser = '@' + m.sender.split("@s.whatsapp.net")[0]
-let av = `./Assets/mp3/${pickRandom(["Guru", "Guru1", "Guru2", "Guru3", "Guru4"])}.mp3`
+let av = `./Assetsss/mp3/${pickRandom(["Guru", "Guru1", "Guru2", "Guru3", "Guru4"])}.mp3`
 
 m.reply( `مرحبا ${taguser} 🔄تحتاج مساعدة  اكتب .اوامر`)
 conn.sendFile(m.chat, av, 'audio.mp3', null, m, true, { type: 'audioMessage', ptt: true })
