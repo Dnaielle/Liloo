4c61fc223619dfaa4d4384a969dff1a05a7abd09
Danielle096 <157294641+Danielle096@users.noreply.github.com>
2024-01-24 20:48:51 +0300

    Update bot.js

--- plugins/bot.js
+++ plugins/bot.js
@@ -1,23 +1,14 @@
-
 let handler = async (m, { conn}) => {
 let user = global.db.data.users[m.sender]
 let name = conn.getName(m.sender)
 let taguser = '@' + m.sender.split("@s.whatsapp.net")[0]
 let av = `./Assets/mp3/${pickRandom(["Guru", "Guru1", "Guru2", "Guru3", "Guru4"])}.mp3`
 
-  text = m.quoted.text;
-  }
-
-  try {
-    m.react(rwait)
-    const { key } = await conn.sendMessage(m.chat, {
-      image: { url: 'https://telegra.ph/file/c3f9e4124de1f31c1c6ae.jpg' },
-      caption: 'Thinking....'
-    }, {quoted: m})
-    conn.sendPresenceUpdate('composing', m.chat);
+m.reply( `Hello ${taguser} Need help?  type /help `)
+conn.sendFile(m.chat, av, 'audio.mp3', null, m, true, { type: 'audioMessage', ptt: true })
 } 
 
-handler.customPrefix = /^(بوت|ناكسو)$/i
+handler.customPrefix = /^(bot|guru)$/i
 handler.command = new RegExp
 
 export default handler
