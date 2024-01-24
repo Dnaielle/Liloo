bc82f5b33454046e8849fb6fe17f540b2ae9fe57
Danielle096 <157294641+Danielle096@users.noreply.github.com>
2024-01-24 20:58:00 +0300

    Update bot.js

--- plugins/bot.js
+++ plugins/bot.js
@@ -4,11 +4,11 @@ let name = conn.getName(m.sender)
 let taguser = '@' + m.sender.split("@s.whatsapp.net")[0]
 let av = `./Assets/mp3/${pickRandom(["Guru", "Guru1", "Guru2", "Guru3", "Guru4"])}.mp3`
 
-m.reply( `Hello ${taguser} Need help?  type /help `)
+m.reply( `مرحبا ${taguser} 🔄تحتاج مساعدة  اكتب .اوامر`)
 conn.sendFile(m.chat, av, 'audio.mp3', null, m, true, { type: 'audioMessage', ptt: true })
 } 
 
-handler.customPrefix = /^(bot|guru)$/i
+handler.customPrefix = /^(بوت|ناكسو)$/i
 handler.command = new RegExp
 
 export default handler
