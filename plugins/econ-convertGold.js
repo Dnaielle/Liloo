82e22abe788b14822b1544f7effd38dd9d791c85
Danielle096 <157294641+Danielle096@users.noreply.github.com>
2024-01-24 21:39:21 +0300

    Update econ-convertGold.js

--- plugins/econ-convertGold.js
+++ plugins/econ-convertGold.js
@@ -8,15 +8,15 @@ let handler = async (m, { conn, command, args }) => {
     global.db.data.users[m.sender].exp -= xppercredit * count
     global.db.data.users[m.sender].credit += count
     conn.reply(m.chat, `
-┌─「 *PAYMENT NOTE* 」
-‣ *Nominal purchase* : + ${count} 
-‣ *Spent* : -${xppercredit * count} XP
+┌─「 *اشعار الشراء* 」
+‣ *الشراء الاسمي* : + ${count} 
+‣ *انفق* : -${xppercredit * count} خبرة
 └──────────────`, m)
-  } else conn.reply(m.chat, `❎ Sorry, you don't have enough *XP* to buy *${count}* Gold\n\n You can get *XP* using the commands from the *games and economy menu*`, m)
+  } else conn.reply(m.chat, `❎ اعتذر, ليس لديك مايكفي من الخبرة *${count}* ذهب\n\nيمكنك الحصول على من *الخبرة* استخدم اوامر التي فل الاوامر*`, m)
 }
 handler.help = ['buy', 'buyall']
 handler.tags = ['economy']
-handler.command = ['buy', 'buyall'] 
+handler.command = ['شراء', 'buyall'] 
 
 handler.disabled = false
 
