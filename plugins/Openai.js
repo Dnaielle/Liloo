ebdd648a8dda48ba61654ef8427d54c037b2a8a2
Danielle096 <157294641+Danielle096@users.noreply.github.com>
2024-02-06 16:51:52 +0300

    Create Openai.js

---
+++ plugins/Openai.js
@@ -0,0 +1,78 @@
+import fetch from 'node-fetch';
+
+let handler = async (m, { text, conn, usedPrefix, command }) => {
+  if (!text && !(m.quoted && m.quoted.text)) {
+    throw `Please provide some text or quote a message to get a response.`;
+  }
+
+  if (!text && m.quoted && m.quoted.text) {
+    text = m.quoted.text;
+  }
+
+  try {
+    m.react(rwait)
+    const { key } = await conn.sendMessage(m.chat, {
+      image: { url: 'https://telegra.ph/file/42483be52c0b0d4e8e6ee.jpg' },
+      caption: 'دعني افكر قليلا....'
+    }, {quoted: m})
+    conn.sendPresenceUpdate('composing', m.chat);
+    const prompt = encodeURIComponent(text);
+
+    const guru1 = `${gurubot}/chatgpt?text=${prompt}`;
+    
+    try {
+      let response = await fetch(guru1);
+      let data = await response.json();
+      let result = data.result;
+
+      if (!result) {
+        
+        throw new Error('يوجد خطا حاول لاحقا');
+      }
+
+      await conn.relayMessage(m.chat, {
+        protocolMessage: {
+          key,
+          type: 14,
+          editedMessage: {
+            imageMessage: { caption: result }
+          }
+        }
+      }, {});
+      m.react(done);
+    } catch (error) {
+      console.error('يوجد خطأ حاول لاحقا:', error);
+
+  
+      const model = 'llama';
+      const senderNumber = m.sender.replace(/[^0-9]/g, ''); 
+      const session = `Ez_Bot_${senderNumber}`;
+      const guru2 = `https://ultimetron.guruapi.tech/gpt3?prompt=${prompt}`;
+      
+      let response = await fetch(guru2);
+      let data = await response.json();
+      let result = data.completion;
+
+      await conn.relayMessage(m.chat, {
+        protocolMessage: {
+          key,
+          type: 14,
+          editedMessage: {
+            imageMessage: { caption: result }
+          }
+        }
+      }, {});
+      m.react(done);
+    }
+
+  } catch (error) {
+    console.error('Error:', error);
+    throw `*خطأ*`;
+  }
+};
+handler.help = ['chatgpt']
+handler.tags = ['AI']
+handler.command = ['ناكسو', 'chatgpt', 'ai', 'gpt'];
+
+export default handler;
+ 
