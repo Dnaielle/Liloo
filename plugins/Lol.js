e2307b4061029fde373fb6bcbed530d78dbb5037
Dani089ru <157052154+Dani089ru@users.noreply.github.com>
2024-02-07 15:53:03 +0300

    Create Lol.js

---
+++ plugins/Lol.js
@@ -0,0 +1,50 @@
+import fetch from 'node-fetch';
+import displayLoadingScreen from '../lib/loading.js'
+
+const endpoint = 'https://v2-guru-indratensei.cloud.okteto.net/perplexity?query=';
+
+let handler = async (m, { text, conn, usedPrefix, command }) => {
+  try {
+    if (!text && !(m.quoted && m.quoted.text)) {
+      throw `ادخل نص للتكلم معي.`;
+    }
+
+    if (!text && m.quoted && m.quoted.text) {
+      text = m.quoted.text;
+    } else if (text && m.quoted && m.quoted.text) {
+      text = `${text} ${m.quoted.text}`;
+      if (m.quoted.text.includes('.aisearch')) {
+        text = text.replace('.aisearch', ''); // 
+      }
+    }
+    let emsg = await conn.sendMessage(m.chat, {text: 'Thinking...'})
+    const prompt = encodeURIComponent(text);
+
+    const response = await fetch(endpoint + prompt);
+
+    if (!response.ok) {
+      throw `Received an error response from the server: ${response.status} - ${response.statusText}`;
+    }
+
+    const data = await response.json();
+    const result = data.response.trim(); 
+    await conn.relayMessage(m.chat, {
+        protocolMessage: {
+          key: emsg.key,
+          type: 14,
+          editedMessage: {
+            conversation: result 
+          }
+        }
+      }, {})
+  } catch (error) {
+    console.error('Error:', error);
+    m.reply(`An error occurred while processing your request. Please try again later.`);
+  }
+};
+handler.help = ['aisearch']
+handler.tags = ['AI']
+handler.command = ['جين', 'ai2']; 
+
+
+export default handler;
