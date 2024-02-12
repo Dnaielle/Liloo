c63ec908812fe49c3d0b260726d374131eaec0b2
Dnaielle <157284901+Dnaielle@users.noreply.github.com>
2024-02-12 18:32:47 +0300

    Update Pla2.js

--- plugins/Pla2.js
+++ plugins/Pla2.js
@@ -1,1 +1,15 @@
+import { pinterest } from '@bochilteam/scraper'
+
+let handler = async(m, { conn, text, usedPrefix, command }) => {
+  if (!text) throw `✳️ ماذا تبحث في بينترست?\n\n📌 Example  : ${usedPrefix + command} Lil Peep`
+  const json = await pinterest(text)
+  conn.sendFile(m.chat, json.getRandom(), 'pinterest.jpg', `
+*▢  Pinterest:*  ${text}
+`.trim(), m)
+}
+handler.help = ['pinterest']
+handler.tags = ['img']
+handler.command = ['بينت'] 
+
+export default handler
 
