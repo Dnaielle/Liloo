bc2b88bdee3c4d9efd5c678c29664c1059765000
Dnaielle <157284901+Dnaielle@users.noreply.github.com>
2024-02-19 22:17:45 +0300

    Update ياسو.js

--- plugins/ياسو.js
+++ plugins/ياسو.js
@@ -1,11 +1,10 @@
 let handler = async (m, { conn }) => {
     let user = global.db.data.users[m.sender]
     let name = conn.getName(m.sender)
-    let taguser = '@' + m.sender.split("@s.whatsapp.net")[0]
-
+    let taguser = '@' + m.sender.split("@s.whatsapp.net")[0] 
     let image = 'https://telegra.ph/file/8ed77669c518ee5d4ed92.png'
 
-    conn.sendFile(m.chat, image, 'image.png', `اهلا صديقي ${taguser} تحتاج مساعدة?  اكتب.اوامر`, m)
+    conn.sendFile(m.chat, image, 'image.png', `اهلا صديقي ${name} تحتاج مساعدة?  اكتب.اوامر`, m)
 }
 
 handler.customPrefix = /^(بوت|ياسو)$/i
